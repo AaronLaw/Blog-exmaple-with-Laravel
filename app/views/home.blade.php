69397e853c0e433e5003a8ad5f2af2a7f82e3d1a
Aaron Law <aaronlaw@gmail.com>
2014-10-26 01:36:34 +0800

    filling some testing content in the home view

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('templates.default')
 
 @section('content')
-    Whooa look at me!
+    <article>
+    	<h2>This is article title</h2>
+    	<p>This is some body</p>
+    	<a href="">Read more &rarr;</a>
+    </article>
+
+        <article>
+    	<h2>This is article title</h2>
+    	<p>This is some body</p>
+    	<a href="">Read more &rarr;</a>
+    </article>
+
+        <article>
+    	<h2>This is article title</h2>
+    	<p>This is some body</p>
+    	<a href="">Read more &rarr;</a>
+    </article>
 @stop('content')
