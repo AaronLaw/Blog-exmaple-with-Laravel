bb5ede2e60da15701f4e323e2f8d94af27c2c624
Aaron Law <aaronlaw@gmail.com>
2014-10-26 01:40:31 +0800

    complete part2 of tutorial

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -6,16 +6,4 @@
     	<p>This is some body</p>
     	<a href="">Read more &rarr;</a>
     </article>
-
-        <article>
-    	<h2>This is article title</h2>
-    	<p>This is some body</p>
-    	<a href="">Read more &rarr;</a>
-    </article>
-
-        <article>
-    	<h2>This is article title</h2>
-    	<p>This is some body</p>
-    	<a href="">Read more &rarr;</a>
-    </article>
 @stop('content')
