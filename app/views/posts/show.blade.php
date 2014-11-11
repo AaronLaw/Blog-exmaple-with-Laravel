d751cb39929c1d2acff767dcbe37531027dcb396
Aaron Law <aaronlaw@gmail.com>
2014-11-12 03:24:37 +0800

    Published on created_at in posts.show.blade.php, with formatting

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -6,6 +6,7 @@
 @section('content')
            <article>
                 <h2>{{ $post->title }}</h2>
+                <p>Published on {{ $post->created_at->format('l jS \\of F Y')}} </p>
                 <p>{{ Markdown::parse($post->body )}}</p>
             </article>
 @stop('content')
